b6b25f26af29256c7c1ff1a9a5478a9b7935fd93
DiaxuDev <diaxman.gameplay@gmail.com>
2026-01-30 17:49:52 +0100

    feat: optimize nix storage space

--- system/common/nix.nix
+++ system/common/nix.nix
@@ -1,14 +1,22 @@
 { ... }:
 {
-  nix.settings.experimental-features = [
-    "nix-command"
-    "flakes"
-  ];
+  nix.settings = {
+    experimental-features = [
+      "nix-command"
+      "flakes"
+    ];
+
+    auto-optimise-store = true;
+  };
 
   nixpkgs.config.allowUnfree = true;
 
   programs.nh = {
     enable = true;
     flake = "/home/diaxudev/.nixos";
+    clean = {
+      enable = true;
+      extraArgs = "--keep 3 --keep-since 7d";
+    };
   };
 }
