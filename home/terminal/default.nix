7d7b902e38a6a8bf6b487cc1ae917a0dac352511
DiaxuDev <diaxman.gameplay@gmail.com>
2026-01-30 17:19:59 +0100

    feat: add file utility

--- home/terminal/default.nix
+++ home/terminal/default.nix
@@ -1,8 +1,12 @@
-{ ... }:
+{ pkgs, ... }:
 {
   imports = [
     ./shell
     ./git.nix
     ./kitty.nix
+  ];
+
+  home.packages = with pkgs; [
+    file # wdym it's not installed by default
   ];
 }
