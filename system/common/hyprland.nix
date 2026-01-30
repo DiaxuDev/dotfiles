83cf926f5d3c01d63f207c5db1fe317dc96baf66
DiaxuDev <diaxman.gameplay@gmail.com>
2026-01-30 17:20:20 +0100

    fix: tell electron apps to use wayland

--- system/common/hyprland.nix
+++ system/common/hyprland.nix
@@ -3,4 +3,5 @@
   programs.hyprland.enable = true;
   services.displayManager.sddm.enable = true;
   services.displayManager.sddm.wayland.enable = true;
+  environment.variables.NIXOS_OZONE_WL = "1";
 }
