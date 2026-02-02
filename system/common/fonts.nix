9edbea082764aad264b8cfcfb5a7fdcddceb6fa3
DiaxuDev <diaxman.gameplay@gmail.com>
2026-02-02 19:31:35 +0100

    fix: fix web fonts

--- system/common/fonts.nix
+++ system/common/fonts.nix
@@ -1,11 +1,14 @@
 { pkgs, ... }:
 {
   fonts = {
+    enableDefaultPackages = false; # this adds a lot of ugly fonts that get matched before my fonts
+
     packages = with pkgs; [
       libertinus
       noto-fonts
       noto-fonts-color-emoji
       nerd-fonts.jetbrains-mono
+      corefonts
     ];
 
     fontconfig.defaultFonts = {
