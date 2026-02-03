8634cc18e0220f10bf77a3a8ea2925f619d634eb
DiaxuDev <diaxman.gameplay@gmail.com>
2026-02-03 08:38:20 +0100

    refactor: move hardcoded workspace keybinds into a generator fuction

--- home/hyprland/default.nix
+++ home/hyprland/default.nix
@@ -6,6 +6,20 @@
 let
   inherit (lib.strings) removePrefix;
   c = config.modules.colors;
+
+  workspaces = builtins.concatLists (
+    builtins.genList (
+      x:
+      let
+        ws = x + 1;
+        key = toString (ws - (10 * (ws / 10)));
+      in
+      [
+        "SUPER, ${key}, workspace, ${toString ws}"
+        "SUPER SHIFT, ${key}, movetoworkspace, ${toString ws}"
+      ]
+    ) 10
+  );
 in
 {
   imports = [
@@ -48,6 +62,11 @@ in
 
       animations.enabled = false;
 
+      bindm = [
+        "SUPER, mouse:272, movewindow"
+        "SUPER, mouse:273, resizewindow"
+      ];
+
       bind = [
         "SUPER, return, exec, kitty"
         "SUPER, r, exec, vicinae toggle"
@@ -57,27 +76,8 @@ in
         "SUPER, right, movefocus, r"
         "SUPER, up, movefocus, u"
         "SUPER, down, movefocus, b"
-
-        "SUPER, 1, workspace, 1"
-        "SUPER, 2, workspace, 2"
-        "SUPER, 3, workspace, 3"
-        "SUPER, 4, workspace, 4"
-        "SUPER, 5, workspace, 5"
-        "SUPER, 6, workspace, 6"
-        "SUPER, 7, workspace, 7"
-        "SUPER, 8, workspace, 8"
-        "SUPER, 9, workspace, 9"
-
-        "SUPER SHIFT, 1, movetoworkspace, 1"
-        "SUPER SHIFT, 2, movetoworkspace, 2"
-        "SUPER SHIFT, 3, movetoworkspace, 3"
-        "SUPER SHIFT, 4, movetoworkspace, 4"
-        "SUPER SHIFT, 5, movetoworkspace, 5"
-        "SUPER SHIFT, 6, movetoworkspace, 6"
-        "SUPER SHIFT, 7, movetoworkspace, 7"
-        "SUPER SHIFT, 8, movetoworkspace, 8"
-        "SUPER SHIFT, 9, movetoworkspace, 9"
-      ];
+      ]
+      ++ workspaces;
     };
   };
 }
