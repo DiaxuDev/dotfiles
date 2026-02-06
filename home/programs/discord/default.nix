1fe4e878586d8cccdfb2407f2111efd9f5c8076d
DiaxuDev <diaxman.gameplay@gmail.com>
2026-02-06 22:00:25 +0100

    feat: enable vencord plugins

--- home/programs/discord/default.nix
+++ home/programs/discord/default.nix
@@ -6,7 +6,20 @@
       themes = {
         system24 = ./system24.css;
       };
-      settings.enabledThemes = [ "system24.css" ];
+      settings = {
+        enabledThemes = [ "system24.css" ];
+
+        plugins = {
+          BetterGifAltText.enabled = true;
+          BetterGifPicker.enabled = true; # TODO: port FavoriteMedia BD plugin to vencord
+          BetterUploadButton.enabled = true;
+          ClearURLs.enabled = true;
+          MessageClickActions.enabled = true;
+          NoF1.enabled = true;
+          QuickReply.enabled = true;
+          TypingTweaks.enabled = true;
+        };
+      };
     };
   };
 }
