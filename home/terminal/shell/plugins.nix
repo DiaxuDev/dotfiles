19a103f4b6905223ce39b0c6c21b106c99465f5b
DiaxuDev <diaxman.gameplay@gmail.com>
2026-02-10 17:42:14 +0100

    feat: add OMZ sudo plugin to zsh

--- home/terminal/shell/plugins.nix
+++ home/terminal/shell/plugins.nix
@@ -1,4 +1,20 @@
 { pkgs, ... }:
 {
-  programs.zsh.plugins = [ ];
+  programs.zsh.plugins = [
+    {
+      name = "omz-sudo";
+      file = "sudo.plugin.zsh";
+      src = pkgs.fetchurl {
+        url = "https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/refs/heads/master/plugins/sudo/sudo.plugin.zsh";
+        hash = "sha256-e5XU1bdWZAVGMAMKcKNXRGEzgwLaYijyCRN23WLJcGk=";
+
+        downloadToTemp = true;
+        recursiveHash = true;
+        postFetch = ''
+          mkdir "$out"
+          mv "$downloadedFile" "$out/sudo.plugin.zsh"
+        '';
+      };
+    }
+  ];
 }
