613033ec78e6676f3417d5e37ea3bc8ceb1dc803
DiaxuDev <diaxman.gameplay@gmail.com>
2026-02-21 20:36:08 +0100

    feat: add support for cjk chars

--- system/common/fonts.nix
+++ system/common/fonts.nix
@@ -6,6 +6,7 @@
     packages = with pkgs; [
       libertinus
       noto-fonts
+      noto-fonts-cjk-sans
       noto-fonts-color-emoji
       nerd-fonts.jetbrains-mono
       nerd-fonts.symbols-only
