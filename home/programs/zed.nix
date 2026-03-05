c9efdd13afad836378411744f4c3785d070ca1df
DiaxuDev <diaxman.gameplay@gmail.com>
2026-03-05 19:41:54 +0100

    feat: update zed settings

--- home/programs/zed.nix
+++ home/programs/zed.nix
@@ -9,6 +9,9 @@
     userSettings = {
       # there is literally a char made specifically for the purpose of indenting content, why do people insist on using spacecs
       hard_tabs = true;
+      load_direnv = "shell_hook";
+      diagnostics.inline.enabled = true;
+
       languages = {
         Nix = {
           language_servers = [
@@ -19,6 +22,7 @@
       };
       lsp = {
         nixd.binary.path = lib.getExe pkgs.nixd;
+        package-version-server.binary.path = lib.getExe pkgs.package-version-server;
       };
     };
   };
