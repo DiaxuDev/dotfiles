f2079358381672bb3692285fc6b536fb92c13804
DiaxuDev <diaxman.gameplay@gmail.com>
2026-04-02 08:59:00 +0200

    feat: add lact

--- system/hosts/nixos.nix
+++ system/hosts/nixos.nix
@@ -11,6 +11,7 @@
   services.xserver.videoDrivers = [ "nvidia" ];
   hardware.nvidia.open = true;
   hardware.nvidia.powerManagement.enable = true;
+  services.lact.enable = true;
 
   # Fix specific suspend/wakeup issue with my motherboard
   # See https://github.com/NixOS/nixos-hardware/blob/master/gigabyte/b550/b550-fix-suspend.nix
