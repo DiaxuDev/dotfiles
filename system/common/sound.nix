fac44795904a3da224c140af45bb00788353d1ea
DiaxuDev <diaxman.gameplay@gmail.com>
2026-04-02 08:57:32 +0200

    fix: attempt to fix horrible audio quality when switching to wireless headset

--- system/common/sound.nix
+++ system/common/sound.nix
@@ -3,6 +3,31 @@
   services.pipewire = {
     enable = true;
     pulse.enable = true;
+
+    # Stolen from NotAShelf in hopes of fixing my spotify sounding like it's under the water
+    wireplumber.extraConfig = {
+      "10-bluez" = {
+        "monitor.bluez.rules" = [
+          {
+            matches = [ { "device.name" = "~bluez_card.*"; } ];
+            actions = {
+              update-props = {
+                "bluez5.roles" = [
+                  "hsp_hs"
+                  "hsp_ag"
+                  "hfp_hf"
+                  "hfp_ag"
+                ];
+                "bluez5.enable-msbc" = true;
+                "bluez5.enable-sbc-xq" = true;
+                "bluez5.enable-hw-volume" = true;
+                "bluez5.a2dp.ldac.quality" = "hq";
+              };
+            };
+          }
+        ];
+      };
+    };
   };
 
   # Pavucontrol was technically made for pulseaudio, but it's here until I find a better alternative
