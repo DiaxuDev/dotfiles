d6c865e63a1338384a553198b7c6c5d7b2bb8929
DiaxuDev <diaxman.gameplay@gmail.com>
2026-06-12 19:35:27 +0200

    style: apply nix fmt

--- hjem/services/wl-clip-persist.nix
+++ hjem/services/wl-clip-persist.nix
@@ -1,10 +1,10 @@
-{lib, pkgs, ...}:
+{ lib, pkgs, ... }:
 {
   hj.systemd.services.wl-clip-persist = {
     description = "Clipboard persistence service";
-    after = ["graphical-session.target"];
-    wantedBy = ["graphical-session.target"];
-    partOf = ["graphical-session.target"];
+    after = [ "graphical-session.target" ];
+    wantedBy = [ "graphical-session.target" ];
+    partOf = [ "graphical-session.target" ];
     serviceConfig = {
       Type = "simple";
       ExecStart = "${lib.getExe pkgs.wl-clip-persist} --clipboard regular";
