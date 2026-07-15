3d3f91c0bd13c1a27854ac14668a06182b33dc3a
DiaxuDev <diaxman.gameplay@gmail.com>
2026-07-15 09:04:59 +0200

    feat: add podman

--- modules/core/virtualization.nix
+++ modules/core/virtualization.nix
@@ -2,14 +2,22 @@
 {
   programs.virt-manager.enable = true;
 
-  virtualisation.libvirtd = {
-    enable = true;
-    qemu = {
-      package = pkgs.qemu_kvm;
-      swtpm.enable = true;
+  virtualisation = {
+    libvirtd = {
+      enable = true;
+      qemu = {
+        package = pkgs.qemu_kvm;
+        swtpm.enable = true;
+      };
+      onBoot = "ignore";
+      onShutdown = "shutdown";
     };
-    onBoot = "ignore";
-    onShutdown = "shutdown";
+
+    podman = {
+      enable = true;
+      defaultNetwork.settings.dns_enabled = true;
+    };
+    containers.registries.search = [ "docker.io" ];
   };
 
   networking.firewall.trustedInterfaces = [ "virbr0" ];
