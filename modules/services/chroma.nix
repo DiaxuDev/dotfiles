b48aa0ad4fda41a93a8ad9361a577d185c42a3a0
DiaxuDev <diaxudev@gmail.com>
2026-08-13 22:09:32 +0200

    chroma: add toggle-wallpaper script
    The famous F8 script from my laptop finally ported to the main PC

--- modules/services/chroma.nix
+++ modules/services/chroma.nix
@@ -42,10 +42,46 @@
     };
   };
 
-  cfg.hyprland.settings = {
-    misc = {
+  cfg.hyprland = {
+    settings.misc = {
       disable_hyprland_logo = true; # disable the default background
       disable_splash_rendering = true; # disable default splash rendering
     };
+
+    binds = [
+      {
+        key = "SUPER + F8";
+        exec =
+          let
+            script = pkgs.writeShellApplication {
+              name = "toggle-wallpaper";
+              runtimeInputs = with pkgs; [
+                netcat
+                jq
+                hyprland
+              ];
+              text = ''
+                monitor=$(hyprctl monitors -j | jq 'first(.[] | select(.focused)).name' -r)
+                current=$(echo "get $monitor" | nc -U /tmp/chroma.sock)
+
+                case $(basename "$(readlink -f "$current")") in
+                    "diaxuchan.png")
+                        new="diaxuchan_alt.png"
+                        ;;
+                    "diaxuchan_alt.png")
+                        new="diaxuchan.png"
+                        ;;
+                    *)
+                        exit 0
+                        ;;
+                esac
+
+                echo "set $monitor ~/.local/share/wallpapers/$new" | nc -U /tmp/chroma.sock
+              '';
+            };
+          in
+          ''hl.dsp.exec_raw("${lib.getExe script}")'';
+      }
+    ];
   };
 }
